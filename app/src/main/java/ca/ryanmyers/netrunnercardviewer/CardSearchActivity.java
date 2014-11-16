c92299eb1a8daa99f43db68ee0404734c7d70016
Ryan Myers <ryan.p.myers@gmail.com>
2014-11-15 19:29:15 -0500

    Get a basic page created.

--- app/src/main/java/ca/ryanmyers/netrunnercardviewer/CardSearchActivity.java
+++ app/src/main/java/ca/ryanmyers/netrunnercardviewer/CardSearchActivity.java
@@ -4,22 +4,45 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.util.JsonReader;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import android.util.Log;
 
 
 public class CardSearchActivity extends ActionBarActivity {
+    private static final String TAG = "NetrunnerCardActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_card_search);
     }
 
+    protected void addCardsToView() {
+        Log.d(TAG, "Clicked Search");
+    }
+
+    protected JSONObject readNetrunnerDB(String api, String code) {
+        StringBuilder builder = new StringBuilder();
+        HttpClient client = new DefaultHttpClient();
+        return new JSONObject();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_card_search, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
+
     }
 
     @Override
@@ -27,13 +50,15 @@ public class CardSearchActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_bar_search:
+                addCardsToView();
+                return true;
+            case R.id.action_bar_settings :
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
